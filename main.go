47fa86472f8bf82686bc4b5a39b3eb94db1ac60e
spea <spea55sugaku@outlook.jp>
2022-03-17 02:21:19 +0900

    定期実行できるようにした

--- main.go
+++ main.go
@@ -1,31 +1,49 @@
 package main
 
 import (
+	"os"
+	"os/signal"
 	"strconv"
+	"time"
+	"fmt"
 
 	"local.packages/bot"
 	"local.packages/getContributions"
 )
 
 func main() {
-	whurl := "" //webhook URL
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, os.Interrupt)
+	tk := time.NewTicker(1 * time.Minute)
+	defer tk.Stop()
+
+	loop:
+        for {
+                select {
+                case <- sc:
+                    fmt.Println("stop bot")
+                    break loop
+                case <-tk.C:
+                    if time.Now().Hour() == 20 {
+						runBot()
+					}
+                }
+        }
+}
+
+func runBot() {
+	getContributions.EnvLoading()
+	whurl := os.Getenv("WEBHOOK") //webhook URL
+	fmt.Println(whurl)
 	dw := &bot.DiscordWebhook{}
 	content := bot.Content{}
 
 	tc := getContributions.GetContributions()
 	content.TodayContributions = strconv.Itoa(int(tc))
-	// if isContribution() {
-	// 	content.Text = "@channel 進捗どうですか？"
-	// }
+	if tc == 0 {
+		content.Text = "@channel 進捗どうですか？"
+	}
 
 	dw.CreateMessage(content)
 	dw.SendMessage(whurl)
 }
-
-// func isContribution(num githubv4.int) bool {
-// 	if num == 0 {
-// 		return true
-// 	}
-
-// 	return false
-// }
